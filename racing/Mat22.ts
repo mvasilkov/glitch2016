280e7de5fb28b09c62f4dd4d4c4060812d04d2a8
Mark Vasilkov <mvasilkov@gmail.com>
2016-08-18 13:58:58 +0300

    Transform

--- racing/Mat22.ts
+++ racing/Mat22.ts
@@ -13,4 +13,19 @@ class Mat22 {
         this.col1.setTo(other.col1)
         this.col2.setTo(other.col2)
     }
+
+    setRot(a: number) {
+        const cos = Math.cos(a)
+        const sin = Math.sin(a)
+
+        this.col1.set(cos, sin)
+        this.col2.set(-sin, cos)
+    }
+
+    static multiplyMatrix(a: Mat22, b: Mat22) {
+        return new Mat22(
+            Vec2.multiplyMatrix(a, b.col1),
+            Vec2.multiplyMatrix(a, b.col2)
+        )
+    }
 }
--- racing/Vec2.ts
+++ racing/Vec2.ts
@@ -22,4 +22,50 @@ class Vec2 {
     copy() {
         return new Vec2(this.x, this.y)
     }
+
+    length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+
+    normalize() {
+        const length = this.length()
+        if (length < Number.MIN_VALUE)
+            return
+
+        const inverseLength = 1 / length
+        this.x *= inverseLength
+        this.y *= inverseLength
+    }
+
+    add(other: Vec2) {
+        this.x += other.x
+        this.y += other.y
+    }
+
+    multiplyScalar(a: number) {
+        this.x *= a
+        this.y *= a
+    }
+
+    multiplyMatrix(mat: Mat22) {
+        this.set(
+            mat.col1.x * this.x + mat.col2.x * this.y,
+            mat.col1.y * this.x + mat.col2.y * this.y
+        )
+    }
+
+    static add(a: Vec2, b: Vec2) {
+        return new Vec2(a.x + b.x, a.y + b.y)
+    }
+
+    static multiplyScalar(a: number, b: Vec2) {
+        return new Vec2(a * b.x, a * b.y)
+    }
+
+    static multiplyMatrix(a: Mat22, b: Vec2) {
+        return new Vec2(
+            a.col1.x * b.x + a.col2.x * b.y,
+            a.col1.y * b.x + a.col2.y * b.y
+        )
+    }
 }
